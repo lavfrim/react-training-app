2cc43993de5a5fb962f6ebbcd8f456098a820e00
Aliaksandr Lazarau <Aliaksandr_Lazarau@epam.com>
2019-10-11 16:27:03 +0300

    feat: create react-training-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Image from "./Components/Image"
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageShadow: 'big',
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <p>Image</p>
+        <Image imageShadow = {this.state.imageShadow} />
+      </>
+    )
+  }
 }
-
-export default App;
